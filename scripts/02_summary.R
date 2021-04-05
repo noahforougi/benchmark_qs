fd782fb75fc5aed07128b9dd1c545d58ff7eff86
Noah Forougi <noahforougi@pa-dhcp-10-120-230-7.gp-vpn.northwestern.private>
2021-04-05 09:55:11 -0700

    fix images

--- scripts/02_summary.R
+++ scripts/02_summary.R
@@ -1,5 +1,10 @@
-# Test the size of the saved object  --------------------------------------------------
-# Make dataframe of saved csv file sizes for each data frame
+library(tidyverse)
+library(here)
+source(file = "https://raw.githubusercontent.com/skhiggins/R_guide/master/scripts/programs/set_theme.R")
+
+save_results <- read.csv(here("results", "save_results.csv"))
+read_results <- read.csv(here("results", "read_results.csv"))
+
 csv_objects <- list.files(here("proc"), full.names = T) %>% 
   tibble(file = .) %>%
   mutate(size = file.size(file)/1e6) %>%
@@ -10,33 +15,78 @@ csv_objects <- list.files(here("proc"), full.names = T) %>%
   filter(file != "read_results" & file != "save_results") %>% 
   arrange(size)
 
-# Visualize ----------------------------------------------------------------------------
-# Plot the time to save each object
-time_saved <- bind_rows(df1_results, df2_results, df3_results, 
-                        df4_results, df5_results, df6_results, 
-                        df7_results, df8_results, df9_results) %>%
-  summary() %>% 
+# Graph the time it takes to read each objects
+read_df <- read_results %>% 
   data.frame() %>%
   transmute(expr, mean = mean/1000) %>% 
-  mutate(func = gsub("\\(d.*$|\\(dat.*$", "", expr))  %>%
+  mutate(func = gsub("\\(d.*$|\\(dat.*$|\\(.*", "", expr))  %>%
   mutate(object = str_extract(string = expr, "df[1-9]"))  %>%
   mutate(file = object) %>%
   left_join(csv_objects, by =  "file") %>%
+  mutate(file = gsub("\\..*$", "", file)) %>%
+  
   rename("csv_size_mb" = "size")%>%
-  mutate("csv_size_mb" = log(csv_size_mb)) %>%
+  mutate("log_csv_size_mb" = log(csv_size_mb)) %>%
+  mutate(log_mean = log(mean)) 
+
+read_df %>%
+  mutate(func = factor(func,levels = c("qread", "read_rds", "read.fst", "readRDS"))) %>% 
   ggplot(aes(x = csv_size_mb, y = mean, group = func, color = func)) + 
   geom_point() + 
   geom_line() + 
+  geom_hline(yintercept = 0) + 
+  #scale_x_continuous(breaks = 1:9, labels = seq(20, 180, 20), expand = expansion(mult = c(0.05,0.05))) +
+  scale_y_continuous(expand = expansion(mult = c(0,0.05))) + 
+  scale_x_continuous(trans = "log", 
+                     labels = scales::label_comma(accuracy = 1), 
+                     expand = expansion(mult = c(0, 0.1))) + 
   set_theme() + 
   ylab("Average time to save (seconds)") +
-  xlab("Log of original csv size (mb)")+ 
+  xlab("Original csv size (mb)") + 
   theme(legend.position = "bottom", 
         legend.title = element_blank())
-ggsave(filename = here("results", "figures", "time_saved.png"), plot = time_saved, device = png())
+
+ggsave(plot = last_plot(), 
+       filename = here("results", "figures", "time_to_read.png"), 
+       width = 7, height = 7)
+
+# Graph the time it takes to save each object
+save_df <- save_results  %>% 
+  data.frame() %>%
+  transmute(expr, mean = mean/1000) %>% 
+  mutate(func = gsub("\\(d.*$|\\(dat.*$|\\(.*", "", expr))  %>%
+  mutate(object = str_extract(string = expr, "df[1-9]"))  %>%
+  mutate(file = object) %>%
+  left_join(csv_objects, by =  "file") %>%
+  mutate(file = gsub("\\..*$", "", file)) %>%
+  rename("csv_size_mb" = "size")%>%
+  mutate("log_csv_size_mb" = log(csv_size_mb)) %>%
+  mutate(log_mean = log(mean)) 
+
+save_df %>%
+  mutate(func = factor(func,levels = c("qsave", "write_rds", "write.fst", "saveRDS"))) %>% 
+  ggplot(aes(x = csv_size_mb, y = mean, group = func, color = func)) + 
+  geom_point() + 
+  geom_line() + 
+  geom_hline(yintercept = 0) + 
+  scale_x_continuous(trans = "log", 
+                     labels = scales::label_comma(accuracy = 1), 
+                     expand = expansion(mult = c(0, 0.1))) + 
+  scale_y_continuous(expand = expansion(mult = c(0,0.05))) + 
+  set_theme() + 
+  ylab("Average time to save (seconds)") +
+  xlab("Original csv size (mb)") + 
+  theme(legend.position = "bottom", 
+        legend.title = element_blank())
+
+ggsave(plot = last_plot(), 
+       filename = here("results", "figures", "time_to_save.png"), 
+       width = 7, height = 7)
 
 
+# Graph the size of each saved object
 # Plot the size of each saved object
-size_saved <- list.files(here("proc"), full.names = T) %>% 
+size_df <- list.files(here("proc"), full.names = T) %>% 
   tibble(file = .) %>%
   mutate(saved_size = file.size(file)/1e6) %>%
   mutate(file = basename(file)) %>% 
@@ -47,37 +97,116 @@ size_saved <- list.files(here("proc"), full.names = T) %>%
   mutate(file = gsub("_.*$", "", file)) %>%
   left_join(csv_objects, by = "file") %>%
   rename("csv_original_size" = "size") %>%
-  mutate(csv_original_size = log(csv_original_size)) %>%
+  mutate(log_csv_original_size = log(csv_original_size)) %>%
+  mutate(log_saved_size = log(saved_size)) %>% 
+  mutate(func = ifelse(func == "writefst", "write.fst", func))
+size_df %>%
+  mutate(func = factor(func,levels = c("qsave", "write_rds", "write.fst", "saveRDS"))) %>% 
   ggplot(aes(x = csv_original_size, y = saved_size, group = func, color = func)) + 
   geom_point() + 
   geom_line() + 
+  geom_hline(yintercept = 0) + 
+  scale_x_continuous(trans = "log", labels = scales::label_comma(accuracy = 1), expand = expansion(mult = c(0.01,0.1))) + 
+  scale_y_continuous(trans = "log", labels = scales::label_comma(accuracy = 1)) + 
+  #scale_x_continuous(breaks = 0:6, labels = round(seq(0, 800, 800/6)), expand = expansion(mult = c(0.01,0.05))) +
+  #scale_y_continuous(breaks = 1:6, labels = seq(800/6, 800, 800/6), expand = expansion(mult = c(0.05,0.05))) +
   set_theme() + 
-  xlab("Log of original csv size (mb)") +
-  ylab("Saved size of .rds or .qs object (mb)") + 
+  xlab("Original csv size (mb)") +
+  ylab("Saved size of object (mb)") + 
   theme(legend.position = "bottom", 
-        legend.title = element_blank())
-ggsave(filename = here("results", "figures", "size_saved.png"), plot = size_saved, device = png())
+        legend.title = element_blank()) 
+
+ggsave(plot = last_plot(), 
+       filename = here("results", "figures", "size_object_saved.png"), 
+       width = 7, height = 7)
 
 
-# Plot the time to read each object
-time_read <- bind_rows(df1_read_results, df2_read_results, df3_read_results, 
-                       df4_read_results, df5_read_results, df6_read_results, 
-                       df7_read_results, df8_read_results, df9_read_results) %>%
-  summary() %>% 
-  data.frame() %>%
-  transmute(expr, mean = mean/1000) %>% 
-  mutate(func = gsub("\\(p.*$|\\(f.*$", "", expr))  %>%
-  mutate(object = str_extract(string = expr, "df[1-9]"))  %>%
-  mutate(file = object) %>%
-  left_join(csv_objects, by =  "file") %>%
-  rename("csv_size_mb" = "size") %>%
-  mutate("csv_size_mb" = log(csv_size_mb)) %>%
-  ggplot(aes(x = csv_size_mb, y = mean, group = func, color = func)) + 
-  geom_point() + 
-  geom_line() + 
-  set_theme() + 
-  ylab("Average time to read (seconds)") +
-  xlab("Log of original csv size (mb)")+ 
-  theme(legend.position = "bottom", 
-        legend.title = element_blank())
-ggsave(filename = here("results", "figures", "time_read.png"), plot = time_read, device = png())
+
+
+
+
+
+
+# # Test the size of the saved object  --------------------------------------------------
+# # Make dataframe of saved csv file sizes for each data frame
+# csv_objects <- list.files(here("proc"), full.names = T) %>% 
+#   tibble(file = .) %>%
+#   mutate(size = file.size(file)/1e6) %>%
+#   mutate(file = basename(file))  %>%
+#   filter(grepl(".csv",file)) %>%
+#   mutate(file = gsub("\\..*$", "", file)) %>%
+#   mutate(file = gsub("sample_", "", file)) %>% 
+#   filter(file != "read_results" & file != "save_results") %>% 
+#   arrange(size)
+# 
+# # Visualize ----------------------------------------------------------------------------
+# # Plot the time to save each object
+# time_saved <- bind_rows(df1_results, df2_results, df3_results, 
+#                         df4_results, df5_results, df6_results, 
+#                         df7_results, df8_results, df9_results) %>%
+#   summary() %>% 
+#   data.frame() %>%
+#   transmute(expr, mean = mean/1000) %>% 
+#   mutate(func = gsub("\\(d.*$|\\(dat.*$", "", expr))  %>%
+#   mutate(object = str_extract(string = expr, "df[1-9]"))  %>%
+#   mutate(file = object) %>%
+#   left_join(csv_objects, by =  "file") %>%
+#   rename("csv_size_mb" = "size")%>%
+#   mutate("csv_size_mb" = log(csv_size_mb)) %>%
+#   ggplot(aes(x = csv_size_mb, y = mean, group = func, color = func)) + 
+#   geom_point() + 
+#   geom_line() + 
+#   set_theme() + 
+#   ylab("Average time to save (seconds)") +
+#   xlab("Log of original csv size (mb)")+ 
+#   theme(legend.position = "bottom", 
+#         legend.title = element_blank())
+# ggsave(filename = here("results", "figures", "time_saved.png"), plot = time_saved, device = png())
+# 
+# 
+# # Plot the size of each saved object
+# size_saved <- list.files(here("proc"), full.names = T) %>% 
+#   tibble(file = .) %>%
+#   mutate(saved_size = file.size(file)/1e6) %>%
+#   mutate(file = basename(file)) %>% 
+#   filter(!grepl(".csv",file)) %>%
+#   mutate(file = gsub("\\..*$", "", file)) %>% 
+#   arrange(file) %>%
+#   mutate(func = gsub("df[1-9]_", "", file))  %>%
+#   mutate(file = gsub("_.*$", "", file)) %>%
+#   left_join(csv_objects, by = "file") %>%
+#   rename("csv_original_size" = "size") %>%
+#   mutate(csv_original_size = log(csv_original_size)) %>%
+#   ggplot(aes(x = csv_original_size, y = saved_size, group = func, color = func)) + 
+#   geom_point() + 
+#   geom_line() + 
+#   set_theme() + 
+#   xlab("Log of original csv size (mb)") +
+#   ylab("Saved size of .rds or .qs object (mb)") + 
+#   theme(legend.position = "bottom", 
+#         legend.title = element_blank())
+# ggsave(filename = here("results", "figures", "size_saved.png"), plot = size_saved, device = png())
+# 
+# 
+# # Plot the time to read each object
+# time_read <- bind_rows(df1_read_results, df2_read_results, df3_read_results, 
+#                        df4_read_results, df5_read_results, df6_read_results, 
+#                        df7_read_results, df8_read_results, df9_read_results) %>%
+#   summary() %>% 
+#   data.frame() %>%
+#   transmute(expr, mean = mean/1000) %>% 
+#   mutate(func = gsub("\\(p.*$|\\(f.*$", "", expr))  %>%
+#   mutate(object = str_extract(string = expr, "df[1-9]"))  %>%
+#   mutate(file = object) %>%
+#   left_join(csv_objects, by =  "file") %>%
+#   rename("csv_size_mb" = "size") %>%
+#   mutate("csv_size_mb" = log(csv_size_mb)) %>%
+#   ggplot(aes(x = csv_size_mb, y = mean, group = func, color = func)) + 
+#   geom_point() + 
+#   geom_line() + 
+#   set_theme() + 
+#   ylab("Average time to read (seconds)") +
+#   xlab("Log of original csv size (mb)")+ 
+#   theme(legend.position = "bottom", 
+#         legend.title = element_blank())
+# ggsave(filename = here("results", "figures", "time_read.png"), plot = time_read, device = png())# 
--- scripts/benchmarking_qs.md
+++ scripts/benchmarking_qs.md
@@ -1,9 +1,19 @@
-# Benchmarking qs() Package
+# Benchmarking qs Package
 
-Here, we test the speed at which the `qsave()` and `qread()` functions from the `qs` package operate relative to the functions `read_rds()`/`readRDS()` and `write_rds()`/`saveRDS()`. There are some benchmarks on the `qs()` package that are available on the `qs` package github repository. However, this is mostly an illustration of the parameterization of the `qs` package functions and how different options change the file size and the speed for a single object. In the following tests, we look at the speed and efficiency of the mentioned packages by looking at the performance of functions across objects of different sizes. 
+Here, we test the speed at which the `qsave()` and `qread()` functions from the `qs` package operate relative to the functions `read_rds()`/ `readRDS()`/ `read.fst()` and `write_rds()`/`saveRDS()`/`write.fst()`. There are some benchmarks on the `qs` package that are available on the `qs` package github repository. However, this is mostly an illustration of the parameterization of the `qs` package functions and how different options change the file size and the speed for a single object. In the following tests, we look at the speed and efficiency of the mentioned packages by looking at the performance of functions across objects of different sizes. 
 
-We first look at the speed of saving objects. We look at the performance of the saving functions relative to the size of the file in a csv form. The graph below shows that the `qsave()` function operates much quicker than `saveRDS()` or `save_rds()` for saving files that range in size from ~2 mb to ~800 mb. The x-axis has been converted to log scale for better visualization. 
+We first look at the speed of saving objects. We look at the performance of the saving functions relative to the size of the file in a csv form. The graphs below shows that the `qsave()` function operates quicker than `saveRDS()`, but not as fast as `save_rds()` or `write.fst()`  for saving files that range in size from ~2 mb to ~800 mb. We also see that `qread()` operates quicker than both the rds functions, while the `fst` package is still quicker. The x-axes has been converted to log scale for better visualization.  
 
-![image](/Users/noahforougi/benchmark_qs/results/figures/saving_time.png)
+An important caveat to these benchmarks is that the `fst` package is currently only designed for dataframes, so saving complex (and often, large) objects like regressions can only be done through the rds or qs functions. 
 
-The other part of the story is looking at the size of the saved .rds or .qs object. The 
+
+Time to Save     | Time to Read
+:-------------------------:|:-------------------------:
+![](/Users/noahforougi/benchmark_qs/results/figures/time_to_save.png) |  ![](/Users/noahforougi/benchmark_qs/results/figures/time_to_read.png)
+
+
+The other part of the story is looking at the size of the saved .rds / .qs / .fst object. In these results, we see that `qsave()` outperforms every other function in terms of minimizing the size of the saved object. While the `write_rds()` and `read_rds()` are comparable to the `qs` versions in terms of speed, the `qsave()` outperforms alternative functions for compressing the objects. 
+
+<p align="center">
+  <img width="500" height="500" src="/Users/noahforougi/benchmark_qs/results/figures/size_object_saved.png">
+</p>
